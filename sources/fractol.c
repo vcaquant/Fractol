082f73acdeee85014c29455d668cb1901bac847d
Victorien CAQUANT <vcaquant@e2r2p8.42.fr>
2016-11-15 17:57:14 +0100

    auto commit

--- sources/fractol.c
+++ sources/fractol.c
@@ -11,3 +11,23 @@
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
+
+int		ft_init_struct(t_env **env, char **av)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	str = av[1];
+	if (!(*env = (t_env*)malloc(sizeof(t_env))))
+		return (0);
+	(*env)->s_win = NULL;
+	return (1);
+}
+
+int     main(int ac, char **av)
+{
+    t_env   *env;
+
+    
+}
